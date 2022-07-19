4cba972ad064756b8785623a8502901eea4b6590
sashagod1986 <sashagod1986@gmail.com>
2022-07-19 21:58:01 +0300

    second

--- radioStation1/src/main/java/org/example/Main.java
+++ radioStation1/src/main/java/org/example/Main.java
@@ -4,6 +4,9 @@ import org.example.radio.RadioEngine;
 
 public class Main {
     public static void main(String[] args) {
+
+        // pattern abstract factory
+
         RadioEngine.radioEngine();
     }
 }
